f5d511f6316a251a8ef932dbe447402741116f5b
wenzstev <wenzelstev@gmail.com>
2020-10-08 10:12:57 -0400

    first test with styled components

---
+++ src/components/MainActionButton.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import styled from "styled-components"
+
+const Button = styled.button`
+  font-size: 20px;
+  text-align: center;
+  color: blue;
+`
+
+const MainActionButton = () => {
+  return (
+    <Button>Test</Button>
+  )
+}
+
+export default MainActionButton
